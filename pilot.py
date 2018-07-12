34e486c488ad172a97be3e2aa0b467032a3ca265
Liu Jiacheng <liujch1998@gmail.com>
2018-07-11 21:57:14 -0500

    Add report

---
+++ pilot.py
@@ -0,0 +1,51 @@
+########### Python 2.7 #############
+import httplib, urllib, base64, json
+
+###############################################
+#### Update or verify the following values. ###
+###############################################
+
+# Replace the subscription_key string value with your valid subscription key.
+subscription_key = 'a988ee9ace8a4a60813762e6b68986d9'
+
+# Replace or verify the region.
+#
+# You must use the same region in your REST API call as you used to obtain your subscription keys.
+# For example, if you obtained your subscription keys from the westus region, replace 
+# "westcentralus" in the URI below with "westus".
+#
+# NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+# a free trial subscription key, you should not need to change this region.
+uri_base = 'westus.api.cognitive.microsoft.com'
+
+headers = {
+    # Request headers.
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscription_key,
+}
+
+params = urllib.urlencode({
+    # Request parameters. All of them are optional.
+    'visualFeatures': 'Categories,Description,Color',
+    'language': 'en',
+})
+
+# The URL of a JPEG image to analyze.
+body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/1/12/Broadway_and_Times_Square_by_night.jpg'}"
+
+try:
+    # Execute the REST API call and get the response.
+    conn = httplib.HTTPSConnection(uri_base)
+    conn.request("POST", "/vision/v1.0/analyze?%s" % params, body, headers)
+    response = conn.getresponse()
+    data = response.read()
+
+    # 'data' contains the JSON data. The following formats the JSON data for display.
+    parsed = json.loads(data)
+    print ("Response:")
+    print (json.dumps(parsed, sort_keys=True, indent=2))
+    conn.close()
+
+except Exception as e:
+    print('Error:')
+    print(e)
